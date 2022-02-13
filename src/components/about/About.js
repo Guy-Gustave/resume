aa7ba5748ec710d0c15e0dfa971d05c5dde659b7
Guy-Gustave <guymoustapha@gmail.com>
2022-02-13 14:03:17 +0200

    Add about component and its style file

---
+++ src/components/about/About.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './about.css'
+
+const About = () => {
+    return (
+        <div className='about'>
+          <div class="about-left">
+            <h3>about 1</h3>
+
+            <h1>About me</h1>
+            <h3 class="full">Full stack Web developer</h3>
+            Committed to bringing business success by deducing solutions to problems, I work on full-stack projects by using several technologies . Working remotely is my favorite option.
+          </div>
+          <div class="about-right">
+            <h3>about 2</h3>
+            <h3>Languages: JavaScript, Ruby, Html, CSS.</h3>
+            <h3>Frameworks: Bootstrap, Ruby on Rails, React.</h3> 
+            <h3>Skills: Git, Github, Heroku, Netlfy, Rspec, Jest.</h3> 
+          </div>
+        </div>
+    );
+};
+
+export default About;
