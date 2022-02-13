0385c787e8862f8ecea35645bb8facbfe2090920
Guy-Gustave <guymoustapha@gmail.com>
2022-02-13 14:05:33 +0200

    Add Assets
    Add dependencies
    
    edit navbar directory

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import About from './components/about/About';
 import Home from './components/home/Home';
 import Navbar from './components/navs/Navbar';
+import Works from './components/works/Works';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
+      <About />
+      <Works />
     </div>
   );
 }
