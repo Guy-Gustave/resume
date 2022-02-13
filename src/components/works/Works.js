f0a7c09426f0a16258c1b3192dd81b5d1e1e8fbb
Guy-Gustave <guymoustapha@gmail.com>
2022-02-13 14:04:37 +0200

    Add new zorks component and its section in app component

---
+++ src/components/works/Works.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './works.css'
+
+const Works = () => {
+    return (
+        <div>
+            <h1 class="portfolio">Portofolio</h1>
+            <div class="w-list">
+              <div class="html">Html</div>
+							<div class="ruby">ruby</div>
+							<div class="rails">Rails</div>
+							<div class="javascript">JavaScript</div>
+							<div class="react">React</div>
+            </div>
+        </div>
+    );
+};
+
+export default Works;
