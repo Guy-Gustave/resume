b98fa76dbe23164373336f3997fe9e6399b88f55
Guy-Gustave <guymoustapha@gmail.com>
2022-02-16 02:48:54 +0200

    aAddnez component of designing loading page with css

---
+++ src/components/presentation/Index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Index.css'
+
+const index = () => {
+    return (
+        <div className='present'>
+          <div class="ring"></div>
+					<div class="ring"></div>
+					<div class="ring"></div>
+          <p>Loading ...</p>
+        </div>
+    );
+};
+
+export default index;
